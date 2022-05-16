7303d0e36da909b90034af7a332309ef5bcb5f73
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2022-05-17 02:27:34 +0300

    Updates

---
+++ precache-manifest.7d1a51279ab8c9c96e085671f409bbcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cea9452e3670b711287",
+    "url": "/weather_widget/static/css/main.8e3cfc60.chunk.css"
+  },
+  {
+    "revision": "2cea9452e3670b711287",
+    "url": "/weather_widget/static/js/main.2cea9452.chunk.js"
+  },
+  {
+    "revision": "afce423140c35331b438",
+    "url": "/weather_widget/static/js/1.afce4231.chunk.js"
+  },
+  {
+    "revision": "d9f79d6777a39f6c9351",
+    "url": "/weather_widget/static/js/runtime~main.d9f79d67.js"
+  },
+  {
+    "revision": "0071796808aff01ad1b16a8df2ec495b",
+    "url": "/weather_widget/index.html"
+  }
+];
